0ac71097bcafc69dd9c6ae1d7e0ec775c1ab08b7
Denys <chornijj14@gmail.com>
2020-06-21 08:41:02 +0200

    Get data my function getData

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-fetch('https://itunes.apple.com/us/rss/topalbums/limit=100/json')
-    .then((res) => {
-      return res.json();
-    })
-    .then( body => {
-      console.log(body)
-    })
+
+const getData = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+}
+getData('https://itunes.apple.com/us/rss/topalbums/limit=100/json')
+  .then(body => {
+    console.log(body);
+  })
