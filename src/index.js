a85ff56f03858565859ebb085b1d765bb3a950e9
Denys <chornijj14@gmail.com>
2020-06-22 13:11:55 +0200

    Created service component with connetction to APi

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
-class ItunesApi{
-  _apiUrl = 'https://itunes.apple.com/us/rss/topalbums/limit=100/json';
-  async getData(url){
-    const res = await fetch(url);
 
-    if(!res.ok){
-      throw new Error(`Could not fetch ${url} recived ${res.status}`);
-    }
-    return await res.json();
-  }
 
-  async getAlbums(){
-    const res = await this.getData(this._apiUrl);
-    return res.feed.entry;
-  }
+// const data = new ItunesService();
 
-}
-
-const data = new ItunesApi();
-
-data.getAlbums().then( albums => {
-  console.log(albums);
-  albums.forEach(album => {
-    console.log(album);
-  });
-});
+// data.getAlbums().then( albums => {
+//   console.log(albums);
+//   albums.forEach(album => {
+//     console.log(album);
+//   });
+// });
