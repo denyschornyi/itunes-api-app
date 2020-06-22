75ccc400400c5918473bd78fb1149b67df84e628
Denys <chornijj14@gmail.com>
2020-06-22 12:37:42 +0200

    Catch API(not found details ) error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 const getData = async (url) => {
   const res = await fetch(url);
+
+  if(!res.ok){
+    throw new Error(`Could not fetch ${url} recived ${res.status}`);
+  }
+  
   const body = await res.json();
   return body;
 }
